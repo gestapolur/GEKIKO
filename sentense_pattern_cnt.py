2fe0bc57f9e10f9c1d249184bd3dab1f20c49320
gestapolur <gestapolur@gmail.com>
2013-12-15 18:57:35 +0800

    add sentense pattern count feature

---
+++ sentense_pattern_cnt.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+from collections import defaultdict
+
+
+is_zh = (lambda x: True if 19968 <= ord(x) <= 40908 else False)
+grammar_map = {'S': ['N'], 'P': ['V','A'], 'O': ['N']}
+
+
+def ptn_cnt(text_buffer, word_list_buffer, s_ptn):
+    #text = ''.join([w for w in ''.join([l for l in text_buffer]) if is_zh(w)])
+    text = ''.join([w for w in ''.join([l for l in text_buffer])])
+    word_list = defaultdict(dict)    
+    for w in word_list_buffer:
+        w = w[:-1]
+        key, weight, tag = w.split(' ')
+        word_list[key]['tag'] = tag.split(',')
+        word_list[key]['weight'] = int(weight)
+    """
+    pattern x could fit on position y
+    """
+    fit_pattern = lambda x, y: True if (
+        y in grammar_map and
+        any(tag in grammar_map[y] for tag in word_list[x]['tag'])
+        ) or (x == y) else False
+
+    tot_ptn = defaultdict(int)
+    for i in range(0, len(text) - len(s_ptn)):
+        ptn = text[i:i+len(s_ptn)]
+        if all(p in word_list for p in ptn):
+            if all(fit_pattern(ptn[j], s_ptn[j])
+                   for j in range(0, len(s_ptn))):
+                tot_ptn[ptn] += 1
+                i += len(ptn)
+    for p in sorted(tot_ptn, key=lambda x: tot_ptn[x], reverse=True):
+        print(p, tot_ptn[p])
+
+#example
+ptn_cnt(open(sys.argv[1], "r"), open(sys.argv[2], "r"), ['P', '于', 'S'])
