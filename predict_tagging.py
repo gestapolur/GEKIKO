d93efb8dfcc49679af9119a17a5b341a37e1ebb9
gestapolur <gestapolur@gmail.com>
2014-05-05 21:07:12 +0800

    revise temp saving

--- predict_tagging.py
+++ predict_tagging.py
@@ -12,6 +12,8 @@ import json
 import re
 from collections import defaultdict
 
+update_similarity = lambda origin_similarity, new_similarity: new_similarity if new_similarity[0] > origin_similarity[0] else origin_similarity
+
 def find_tagable_char(sentence, word_dict, pattern):
 
     def similarity(s_idx, p_idx, p_cnt, similarity_value, match_result):
@@ -23,22 +25,20 @@ def find_tagable_char(sentence, word_dict, pattern):
         print (s_idx, "current pattern position", p_idx, "pattern name", pattern[p_idx])
         print (s_idx, p_idx, similarity_value, match_result, sentence[s_idx], word_dict[sentence[s_idx]])
         new_similarity_result = [similarity_value, match_result]
-        for p_cnt in range(0, pattern[p_idx][1]+1):
-            if p_cnt < len(pattern[p_idx]) and p_idx+p_cnt < len(pattern):
-                #print (sentence[s_idx], pattern[p_idx+p_cnt][0], '!')
-                if (pattern[p_idx+p_cnt][0] in word_dict[sentence[s_idx]].get("tag", [])) or (
-			pattern[p_idx+p_cnt][0] == sentence[s_idx]): # pattern is a specific char
-                    print (p_cnt, match_result, sentence[s_idx])
-                    if not match_result:
-                        match_result = [0]
-                    temp_similarity_result = similarity(
-                        s_idx+p_cnt+1, p_idx, p_cnt+1, similarity_value+1, match_result[:-1]+[match_result[-1]+1])
-                else: # not match
-                    temp_similarity_result = similarity(
-                        s_idx+p_cnt+1, p_idx+1, 0, similarity_value, match_result+[0])
-                if temp_similarity_result[0] > new_similarity_result[0]:
-                    new_similarity_result = temp_similarity_result
-
+        if p_idx < len(pattern):
+            #print (sentence[s_idx], pattern[p_idx+p_cnt][0], '!')
+            if p_cnt < len(pattern[p_idx]) and (pattern[p_idx][0] in word_dict[sentence[s_idx]].get("tag", [])) or (
+		    pattern[p_idx][0] == sentence[s_idx]): # pattern is a specific char
+                print (p_cnt, match_result, sentence[s_idx])
+                if not match_result:
+                    match_result = [0]
+                temp_similarity_result = similarity(
+                    s_idx+1, p_idx, p_cnt+1, similarity_value+1, match_result[:-1]+[match_result[-1]+1])
+                new_similarity_result = update_similarity(new_similarity_result, temp_similarity_result)
+            # not match, stay blank on this position
+            temp_similarity_result = similarity(
+                s_idx+1, p_idx+1, 0, similarity_value, match_result+[0])
+        new_similarity_result = update_similarity(new_similarity_result, temp_similarity_result)
         return new_similarity_result
 
     # sentence has no similarity with pattern
